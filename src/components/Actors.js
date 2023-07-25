9ee22a6928f2f217458426ae47511e966c732b12
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-07-25 15:19:34 +0300

    Completed Assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor) => (
+        <div key={actor.id}>
+          <h2>Name:{actor.name}</h2>
+          <ul key={`${actor.id}_movies`}>
+            {actor.movies.map((movie) => (
+              <li key={movie.id}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Actors;
+
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h2>Title:{movie.title}</h2>
+          <h3>Time{movie.time}</h3>
+          <ul key={`${movie.id}_genres`}>
+            {movie.genres.map((genre) => (
+              <li key={genre}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
+
+
